ffb575121c115b2f4ce3938a4a40a838d4a4fa14
Mário Zola <mariozola69@gmail.com>
2026-07-09 18:38:59 -0300

    Corrigir página membro para deploy

---
+++ frontend/app/membro/page.tsx
@@ -0,0 +1,9 @@
+export default function MembroPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Área do Membro
+      </h1>
+    </main>
+  );
+}
